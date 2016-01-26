2a09f0ca9d9291de5edd143d1eade33db208e3cc
Miguel Fiandor <miguel.fiandor.gutierrez@gmail.com>
2016-01-26 09:07:16 +0100

    start coding

--- src/pandas_cli/__main__.py
+++ src/pandas_cli/__main__.py
@@ -1,16 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
 import sys
 import click
+import pandas as pd
+import logging
 # Why does this file exist, and why __main__?
 # For more info, read:
 # - https://www.python.org/dev/peps/pep-0338/
 # - https://docs.python.org/2/using/cmdline.html#cmdoption-m
 # - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 
+__author__ = 'miguelfg'
+
+
+logger = logging.getLogger('pandas_cli')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger.addHandler(ch)
+
 
 @click.command()
-@click.argument('names', nargs=-1)
-def main(names):
-    click.echo(repr(names))
+@click.argument('path', nargs=-1, default=None)
+def main(path):
+    """
+    """
+    df = pd.read_csv(path)
+    click.echo(df.head(10))
 
 if __name__ == "__main__":
     sys.exit(main())
+
